647da41358ec879fbb530cb9e7ccbc76cc8efffc
Kayki Letieri <54692628+Kayki7@users.noreply.github.com>
2022-04-02 14:56:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const resultado = document.querySelector('.result');
 const confirmar = document.querySelector('.igual');
 
+console.log("Calculator..");
+
 function insert(valor){
     resultado.innerHTML += valor;
 }
